734b03226f0e39df26634b67a9add2200bbba3a1
Rajan Chaudhari <rajanchaudharigits@gmail.com>
2023-03-21 19:00:33 +0530

    :bulb: Importing React Hooks, Mantine UI Core Components and Mantine Hooks from React, React Component Library: Mantine UI Core and Mantine Hooks in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,56 @@
-import { Fragment } from "react";
+/* Import React.js Hooks */
+
+import { Fragment, useState, useRef, useEffect } from "react";
+
+/* Import Mantine Core Components */
+
+import {
+  Modal,
+  Group,
+  Container,
+  Card,
+  Code,
+  Text,
+  TextInput,
+  Button,
+  Title,
+  ActionIcon,
+} from "@mantine/core";
+
+import { MantineProvider, ColorSchemeProvider } from "@mantine/core";
+
+/* Import Mantine Hooks (State & UI Management)  */
+
+import { useColorScheme, useHotkeys, useLocalStorage } from "@mantine/hooks";
+
+/* Import Tabler Icons */
+
+import { Sun, MoonStars, Trash } from "tabler-icons-react";
 
 const App = () => {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <ColorSchemeProvider>
+        <MantineProvider>
+          <Modal>
+            <TextInput></TextInput>
+            <TextInput></TextInput>
+            <Group>
+              <Button></Button>
+              <Button></Button>
+            </Group>
+          </Modal>
+          <Container>
+            <Group>
+              <Title></Title>
+              <ActionIcon></ActionIcon>
+            </Group>
+            <Button>Create Task</Button>
+          </Container>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </Fragment>
+  );
 };
 
 export default App;
