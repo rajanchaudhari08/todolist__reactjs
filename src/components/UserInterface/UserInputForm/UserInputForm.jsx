4bb922a8fd58b73896a27012bf943f67dbbe44e7
Rajan Chaudhari <rajanchaudharigits@gmail.com>
2023-03-24 19:22:56 +0530

    :hammer: UserInputForm.jsx

---
+++ src/components/UserInterface/UserInputForm/UserInputForm.jsx
@@ -0,0 +1,15 @@
+/* Import React Hooks */
+
+/* Import Mantine Core Components */
+
+/* Import Mantine Form Hooks */
+
+const UserInputForm = () => {
+  return (
+    <Fragment>
+      <form></form>
+    </Fragment>
+  );
+};
+
+export default UserInputForm;
